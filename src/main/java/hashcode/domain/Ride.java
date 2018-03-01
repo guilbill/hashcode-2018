226256ba6ddd2004de5dba1442b926947b757f97
jthomas <jthomas@PHLT0125.groupe-welcoop.com>
2018-03-01 19:05:04 +0100

    Ride

--- src/main/java/hashcode/domain/Ride.java
+++ src/main/java/hashcode/domain/Ride.java
@@ -4,4 +4,12 @@ import lombok.Data;
 
 @Data
 public class Ride {
+
+	private int index;
+	private int rowStart;
+	private int columnStart;
+	private int rowEnd;
+	private int columnEnd;
+	private int earliestStart;
+	private int latestFinish;
 }
